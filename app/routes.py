71c169f2c50ffef8a5ce288b566b5c1fb1e96988
05604530190 <robert.sousa@seduh.df.gov.br>
2023-02-15 08:38:14 -0300

    first commit

---
+++ app/routes.py
@@ -0,0 +1,14 @@
+from app import app
+from flask import render_template
+
+@app.route('/')
+@app.route('/index')
+def index():
+    nome = "Robert"
+    dados = {"profissao":"Desenvolvedor", "Idade":"28 anos"}
+    return render_template('index.html', nome= nome, dados=dados)
+
+@app.route('/contato')
+def contato():
+    return render_template('contato.html')
+
